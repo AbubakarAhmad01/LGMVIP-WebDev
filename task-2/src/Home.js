942a030572022f41ac24829efb3ac3805bec581f
Abubakar Ahmad <89373872+AbubakarAhmad01@users.noreply.github.com>
2021-09-20 20:38:13 +0530

    Add files via upload

---
+++ task-2/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+
+export default function Home(){
+    return(
+        <section className="home">
+            <div className="content">
+               
+            </div>
+           
+        </section>
+    )
+}
